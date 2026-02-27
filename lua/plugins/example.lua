887eb881a34d2f75b0a582ec6f3aa48885bc2295
Your Name <you@example.com>
2026-02-27 11:11:26 +0700

    fiks tiga

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -28,26 +28,4 @@ return {
       table.insert(opts.sources, { name = "emoji" })
     end,
   },
-
-  -- Contoh konfigurasi telescope
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      {
-        "<leader>fp",
-        function()
-          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
-        end,
-        desc = "Find Plugin File",
-      },
-    },
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-      },
-    },
-  },
 }
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      {
+        "<leader>fp",
+        function()
+          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+        end,
+        desc = "Find Plugin File",
+      },
+    },
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = {
+          horizontal = {
+            prompt_position = "top",
+            preview_width = 0.55,
+            width = 0.8,
+          },
+        },
+        sorting_strategy = "ascending",
+        winblend = 0,
+
+        -- Previewer untuk code
+        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+      },
+    },
+  },
+}
